b3b2b7d8f116fb36327c3e7821bb8c57881fde73
Avinash <avinash.shetty@sciencelogic.com>
2021-03-27 12:52:15 +0530

    Updating test for weather shopper application

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# weather-shopper-selenium
-weather shopper selenium
+# weather-shopper-selenium-python
+Sample test to automate weather shopper application (http://weathershopper.pythonanywhere.com/) using selenium and python.
--- weather-shopper.py
+++ weather-shopper.py
@@ -1,53 +1,59 @@
 """
-Sample script to solve the moisturizer problem.
-
-Find the least expensive mositurizers with Almond and Aloe in name
+Sample script to test weather shopper application.
+Get the least expensive mositurizers with Almond and Aloe in their product name
 """
 
 from selenium import webdriver
 import time 
 
 # Create an instance of Firefox WebDriver
-browser = webdriver.Chrome()
+driver = webdriver.Firefox()
 
-#navigate
-browser.get('http://weathershopper.pythonanywhere.com/moisturizer')
-print browser.title
+# Navigate to weatheshopper main page 
+driver.get('http://weathershopper.pythonanywhere.com/')
 
-moisturisers = browser.find_elements_by_xpath("//div[contains(@class,'text-center col-4')]")
-aloe_min_element = None 
-almond_min_element = None 
-aloe_min_price = 100000
-almond_min_price = 100000 
+# Click on Buy moisturizer button and check if the title of page is proper
+driver.find_element_by_xpath("//button[text()='Buy moisturizers']").click()
+print(driver.title)
+assert driver.title == "The Best Moisturizers in the World!"
 
-#Find the least expensive aloe and almond moisturizer
-for moisturiser in moisturisers:
-     name =  moisturiser.find_element_by_xpath("p[contains(@class,'font-weight-bold top-space-10')]")
+# Get all moisturizer element
+moisturizers = driver.find_elements_by_xpath("//div[contains(@class,'text-center col-4')]")
+aloe_min_price = 999999
+almond_min_price = 999999
+aloe_min_price_element = None 
+almond_min_price_element = None
+time.sleep(10)
+# Find the least expensive aloe vera and almond moisturizer
+for moisturizer in moisturizers:
+     name =  moisturizer.find_element_by_xpath("p[contains(@class,'font-weight-bold top-space-10')]")
      name = name.text.lower()
-     price = moisturiser.find_element_by_xpath("p[contains(text(),'Price')]")
+     price = moisturizer.find_element_by_xpath("p[contains(text(),'Price')]")
      price = price.text 
      price = price.split('Price:')[-1].strip()
      price = price.split('Rs.')[-1].strip()
-     print price 
      price = int(price)
      
      if 'aloe' in name:
           if price < aloe_min_price:
-               aloe_min_element = moisturiser
+               aloe_min_price_element = moisturizer
                aloe_min_price = price 
      if 'almond' in name:
           if price <  almond_min_price:
-               almond_min_element = moisturiser
+               almond_min_price_element = moisturizer
                almond_min_price = price 
                
-print 'Almond min: ',almond_min_price
-print 'Aloe min: ',aloe_min_price
-aloe_min_element.find_element_by_xpath("button[text()='Add']").click()
-almond_min_element.find_element_by_xpath("button[text()='Add']").click()
+print ('Almond min: ',almond_min_price)
+print ('Aloe min: ',aloe_min_price)
 
-#click add to cart button
-browser.find_element_by_xpath("//button[contains(@class,'nav-link')]").click()
+# Add min price product to cart
+aloe_min_price_element.find_element_by_xpath("button[text()='Add']").click()
+almond_min_price_element.find_element_by_xpath("button[text()='Add']").click()
+
+# Go to cart link
+driver.find_element_by_xpath("//button[contains(@class,'nav-link')]").click()
 
 time.sleep(5)
 
-browser.quit()
+# Quit driver
+driver.quit()
